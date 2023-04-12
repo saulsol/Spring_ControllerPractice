1aa875a90913814998e840766015ef63b89bcd37
saul <dlathf3210@naver.com>
2023-04-12 15:56:48 +0900

    validation update

--- src/main/java/com/example/webmvcpractice/controller/ValidationController.java
+++ src/main/java/com/example/webmvcpractice/controller/ValidationController.java
@@ -1,6 +1,7 @@
 package com.example.webmvcpractice.controller;
 
 import com.example.webmvcpractice.dto.ValidationDTO;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,10 @@ import javax.validation.Valid;
 public class ValidationController {
 
     @GetMapping
-    public String validationTestGetMapping(@Valid @RequestBody ValidationDTO validationDTO){
+    public String validationTestGetMapping(@Validated @RequestBody ValidationDTO validationDTO){
         return "내용 잘 받았습니당";
     }
-
+    // Spring validation 을 통과하지 못하는 경우, Bad Request 가 나가게 된다.
 
 
 
