6f45b5477b0f64d75d70a7f61619f40f87295f1d
saul <dlathf3210@naver.com>
2023-06-01 19:12:23 +0900

    S3 service Start

---
+++ src/main/java/com/example/webmvcpractice/S3Config/S3Service.java
@@ -0,0 +1,11 @@
+package com.example.webmvcpractice.S3Config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class S3Service {
+
+
+
+
+}
---
+++ src/main/java/com/example/webmvcpractice/controller/S3Controller.java
@@ -0,0 +1,13 @@
+package com.example.webmvcpractice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class S3Controller {
+
+
+
+
+
+
+}
